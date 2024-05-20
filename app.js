845c50f236eea558cb1eec4e941c5d9e5c30c99c
Nuari Williams <nuariwilliams@gmail.com>
2024-05-20 18:05:53 -0400

    Change variable spelling

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const campsitesRouter = require('./routes/campsiteRouter');
-const promotionsRouter = require('./routes/promotionRouter');
-const partnersRouter = require('./routes/partnerRouter')
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter')
 
 
 var app = express();
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/campsites', campsitesRouter);
-app.use('/promotions', promotionsRouter);
-app.use('/partners', partnersRouter)
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
